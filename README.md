6ac80491c6d387a1bc37030b8c18b95a696a013e
Andi <andiwildan81@gmail.com>
2025-11-19 18:56:42 +0700

    Initial commit: add HTML, CSS, JS files

---
+++ README.md
@@ -0,0 +1,3 @@
+kalo mau ubh ubh css/js sesuaiin aja sama bagian kalian, css sama js ada di folder terpisah
+udh ada namanya cari sendiri aja ah
+kalo mau masukin gambar taroh aja di folder img
---
+++ assets/js/global.js
@@ -0,0 +1,17 @@
+// AUTO SET ACTIVE SESUAI URL
+const currentPage = location.pathname.split("/").pop();
+const navLinks = document.querySelectorAll(".nav-menu a");
+
+navLinks.forEach(link => {
+  if (link.getAttribute("href") === currentPage) {
+    link.classList.add("active");
+  }
+});
+
+// HAMBURGER RESPONSIVE
+const toggle = document.getElementById("navToggle");
+const navMenu = document.querySelector(".nav-menu");
+
+toggle.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
