7c4ff7a8616babd73194169f66dd7157cd25301d
diahtri31 <diah3utamii@gmail.com>
2025-11-25 11:48:03 +0700

    part 1

---
+++ assets/js/home.js
@@ -0,0 +1,12 @@
+/ assets/js/home.js
+document.querySelectorAll('.cta-button').forEach(btn => {
+    btn.addEventListener('mousedown', ()=>{
+        btn.style.transform = 'scale(0.97)';
+    });
+    btn.addEventListener('mouseup', ()=>{
+        btn.style.transform = 'scale(1)';
+    });
+    btn.addEventListener('mouseleave', ()=>{
+        btn.style.transform = 'scale(1)';
+    });
+});
